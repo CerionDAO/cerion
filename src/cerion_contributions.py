b5b3d660efa024e46e1770088bb535e0d4278e92
CerionDAO <andreww.davis1999@gmail.com>
2025-03-12 08:48:34 +0100

    Create cerion_contributions.py

---
+++ src/cerion_contributions.py
@@ -0,0 +1,16 @@
+### cerion_contributions.py
+
+```python
+# Guidelines for contributing to Cerion
+
+class ContributionGuide:
+    @staticmethod
+    def how_to_contribute():
+        steps = [
+            "Fork the repository on GitHub",
+            "Clone your fork locally",
+            "Create a new feature branch",
+            "Make your modifications and commit changes",
+            "Push the branch and submit a pull request"
+        ]
+        return "\n".join(steps)
